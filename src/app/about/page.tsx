ce8c6f77bf69ed3ae032c275b01a7acd0a5dbf55
junyoung <jadw9715@gmail.com>
2023-05-29 03:15:08 +0900

    feat. about page, contact page layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,28 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <p>About</p>;
+  return (
+    <>
+      <Hero />
+      <section className="p-8 m-8 text-center bg-gray-100 shadow-lg">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 사랑하는 풀스택 개발자 <br />
+          사람과 디자인을 담는 웹앱을 만들고 있음
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          구글 (-Now)
+          <br />
+          페이스북 (-2019)
+          <br />
+          삼전 (-2015)
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>React, Vue, Node</p>
+      </section>
+    </>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,30 @@
+import Contactform from "@/components/Contactform";
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+const LINKS = [
+  { icon: <AiFillGithub />, url: "" },
+  { icon: <AiFillLinkedin />, url: "" },
+  { icon: <AiFillYoutube />, url: "" },
+];
 export default function ContactPage() {
-  return <p>contact</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="my-2 text-3xl font-bold">Contact Me</h2>
+      <p>jade15@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, idx) => (
+          <a
+            key={idx}
+            href={link.url}
+            target="_blank"
+            rel="noreferer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="my-2 text-3xl font-bold">Or Send me an email</h2>
+      <Contactform />
+    </section>
+  );
 }
