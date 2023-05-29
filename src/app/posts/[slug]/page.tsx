c67ba1566643fd53542cf33cd7bb3ca21ff795ad
junyoung <jadw9715@gmail.com>
2023-05-29 23:00:25 +0900

    Refactor. 최적화

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import AdjacentPostCard from "@/components/AdjacentPostCard";
 import PostContent from "@/components/PostContent";
-import { getPostData } from "@/service/posts";
+import { getFeaturedPosts, getPostData } from "@/service/posts";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -39,4 +39,9 @@ export default async function PostPage({ params: { slug } }: Props) {
       </section>
     </article>
   );
+}
+
+export async function generateStaticParams() {
+  const posts = await getFeaturedPosts();
+  return posts.map((post) => ({ slug: post.path }));
 }
